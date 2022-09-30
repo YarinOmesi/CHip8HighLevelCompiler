a644ee6494bcf50e9f03157a7fd98764b4f693c3
Yarin Omesi <yarinomesi@gmail.com>
2022-09-30 18:24:32 +0300

    Added Assembly Parser interface and impl

---
+++ Chip8Compiler.Assembly.Parsing.Base/AssemblyAntlrParser.cs
@@ -0,0 +1,20 @@
+﻿using Antlr4.Runtime;
+using Chip8Compiler.Assembly.Parsing.Core;
+using Chip8Compiler.Assembly.Parsing.Models;
+
+namespace Chip8Compiler.Assembly.Parsing.Base.AntlrParser;
+
+public class AssemblyAntlrParser : IAssemblyParser
+{
+    private readonly IProgramCreator _programCreator = new ProgramCreator();
+
+    public AssemblyProgram Parse(string code)
+    {
+        var inputStream = new AntlrInputStream(code);
+        var lexer = new Chip8AssemblyLexer(inputStream);
+        var commonTokenStream = new CommonTokenStream(lexer);
+        var parser = new Chip8AssemblyParser(commonTokenStream);
+        AssemblyProgram program = _programCreator.Create(parser.program());
+        return program;
+    }
+}
---
+++ Chip8Compiler.Assembly.Parsing.Core/IAssemblyParser.cs
@@ -0,0 +1,8 @@
+﻿using Chip8Compiler.Assembly.Parsing.Models;
+
+namespace Chip8Compiler.Assembly.Parsing.Core;
+
+public interface IAssemblyParser
+{
+    AssemblyProgram Parse(string code);
+}
